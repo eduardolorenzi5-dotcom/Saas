9b7af3a84de3dd9ddeac43a7c877e9237b005f5b
Eduardo <eduardo.lorenzi5@gmail.com>
2026-05-02 22:20:26 -0300

    corrige porta 8080

--- app.py
+++ app.py
@@ -254,4 +254,4 @@ def gerar_relatorio(cliente_id):
 
 if __name__ == "__main__":
     init_db()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
